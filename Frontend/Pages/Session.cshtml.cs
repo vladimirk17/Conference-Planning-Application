6926acb8afefd94cc891fa473fefdc7544cab2f7
Volodymyr Kovieshnikov <vladimirk17@gmail.com>
2021-05-12 16:28:14 +0300

    Added pages for Sessions and Speakers

---
+++ Frontend/Pages/Session.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ConferenceDTO;
+using Frontend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend.Pages
+{
+    public class SessionModel : PageModel
+    {
+        private readonly IAPIClient _apiClient;
+
+        public SessionModel(IAPIClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public SessionResponse Session { get; set; }
+
+        public int? DayOffset { get; set; }
+        
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            Session = await _apiClient.GetSessionAsync(id);
+
+            if (Session == null)
+            {
+                return RedirectToPage("/Index");
+            }
+
+            var allSessions = await _apiClient.GetSessionsAsync();
+
+            var startDate = allSessions.Min(s => s.StartTime?.Date);
+
+            DayOffset = Session.StartTime?.Subtract(startDate ?? DateTimeOffset.MinValue).Days;
+
+            return Page();
+        }
+    }
+}
---
+++ Frontend/Pages/Speaker.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using ConferenceDTO;
+using Frontend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Frontend.Pages
+{
+    public class SpeakerModel : PageModel
+    {
+        private readonly IAPIClient _apiClient;
+
+        public SpeakerModel(IAPIClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        public SpeakerResponse Speaker { get; set; }
+        
+        public async Task<IActionResult> OnGet(int id)
+        {
+            Speaker = await _apiClient.GetSpeakerAsync(id);
+
+            if (Speaker == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+    }
+}

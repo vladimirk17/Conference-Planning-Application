60d59df827014aa500633ba20aaae43e2a9b7fbf
Volodymyr Kovieshnikov <vladimirk17@gmail.com>
2021-05-12 09:39:52 +0300

    Amendments to Speaker controller considering mapping

---
+++ BackEnd/Infrastructure/EntityExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace BackEnd.Data
+{
+    public static class EntityExtensions
+    {
+        /// <summary>
+        /// Маппінг моделі доповідача із переліком сесій
+        /// </summary>
+        /// <param name="speaker"></param>
+        /// <returns></returns>
+        public static ConferenceDTO.SpeakerResponse MapSpeakerResponse(this Speaker speaker) =>
+            new ConferenceDTO.SpeakerResponse
+            {
+                Id = speaker.Id,
+                Name = speaker.Name,
+                Bio = speaker.Bio,
+                WebSite = speaker.WebSite,
+                Sessions = speaker.SessionSpeakers?
+                    .Select(ss => new ConferenceDTO.Session
+                    {
+                        Id = ss.SessionId,
+                        Title = ss.Session.Title
+                    })
+                    .ToList()
+            };
+    }
+}
